f749ed7efc2cabde47a91d6fee450a3ad4bd0d56
Cooks-Johns <cooks.johns@gmail.com>
2022-01-03 14:03:11 -0600

    Creating sheets

--- excel-tools/working.py
+++ excel-tools/working.py
@@ -1,8 +1,7 @@
 from openpyxl import Workbook, load_workbook
 
 wb = load_workbook('sa-voluteer.xlsx')
-ws = wb.active  # worksheet
 
-# When you are updating you must have the file closed inorder for this to work
-ws["A2"].value = "Animal Care Services Department"
-wb.save('sa-voluteer.xlsx')
+# create a new sheet and then print out the sheets
+wb.create_sheet("Test")
+print(wb.sheetnames)
