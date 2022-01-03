d3d515d0638abca40c4c34585a6b07950d5a1306
Cooks-Johns <cooks.johns@gmail.com>
2022-01-03 13:53:49 -0600

    Saving workbooks

--- excel-tools/working.py
+++ excel-tools/working.py
@@ -2,7 +2,7 @@ from openpyxl import Workbook, load_workbook
 
 wb = load_workbook('sa-voluteer.xlsx')
 ws = wb.active  # worksheet
-print(ws['A2'].value) # to get the value of a cell
 
-
-d
+# When you are updating you must have the file closed inorder for this to work
+ws["A2"].value = "Animal Care Services Department"
+wb.save('sa-voluteer.xlsx')
