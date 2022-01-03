9faf9d3ce4714415c67a6c1d6eb0676c919dcd52
Cooks-Johns <cooks.johns@gmail.com>
2022-01-03 15:34:32 -0600

    loops and learning how to formate data when printing

--- excel-tools/loopCells.py
+++ excel-tools/loopCells.py
@@ -1,14 +1,19 @@
-import datetime
-import openpyxl
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
 
-wb = openpyxl.Workbook()
+wb = load_workbook('sa-voluteer.xlsx')
 ws = wb.active
 
-# This will create a defualt workbook meaning there is always one
-ws.title = "Items"
-date_and_time = datetime.datetime.now()
+for row in range(1, 11): # Main row
+    for col in range(1, 5): # nested to access columns 1-4
+        char = get_column_letter(col)
+        print(ws[char + str(row)].value, ", ")
+
+#todo think of a better way to format
+       #  This way
+       # char = chr(65 + col) # cell refrence for int but you need A1 A2 B3 and so on
+       #  # gives the char that is representive of a int 65+col0 = A1, 65+col1 = B1
+       #
 
 
-
-wb.save('items.xlsx')
 
