76e47a001068521d8d5c2ef2d75c90dc75d600e2
Cooks-Johns <cooks.johns@gmail.com>
2022-01-03 14:45:14 -0600

    Making new sheets and adding data

--- excel-tools/working.py
+++ excel-tools/working.py
@@ -1,7 +1,26 @@
+import datetime
+import math
+
+import openpyxl
 from openpyxl import Workbook, load_workbook
 
-wb = load_workbook('sa-voluteer.xlsx')
+wb = openpyxl.Workbook()
+ws = wb.active
 
-# create a new sheet and then print out the sheets
-wb.create_sheet("Test")
-print(wb.sheetnames)
+# This will create a defualt workbook meaning there is always one
+ws.title = "Items"
+wb.iso_dates = True
+
+date_and_time = datetime.datetime.now()
+
+
+ws.append(['testing', 'this ', 'data', date_and_time])
+ws.append(['testing2', 'this ', 'data', date_and_time])
+ws.append(['testing3', 'this ', 'data', date_and_time])
+ws.append(['testing4', 'this ', 'data', date_and_time])
+ws.append(['fin'])
+
+
+
+wb.save('items.xlsx')
+
