537ef61c00e3f4a719a3eca2dd86d53a443e05de
Cooks-Johns <cooks-johns@gmail.com>
2022-01-04 01:38:52 -0600

    Just keeping these lil guys around

---
+++ lab/caloriesBurned.py
@@ -0,0 +1,41 @@
+# Todo
+
+# testing
+# 49
+# 155
+# 148
+# 60
+# outPut-> Calories: 736.21 calories
+
+# todo old
+# age = int(input())
+# weight = float(input())
+# heartRate = float(input())
+# time = float(input())
+#
+# women_calories_str = ((age * 0.074) - (weight * 0.05741) + (heartRate * 0.4472) - 20.4022) * time / 4.184
+# men_calories_str = ((age * 0.2017) + (weight * 0.09036) + (heartRate * 0.6309) - 55.0969) * time / 4.184
+#
+# women_calories = "{:.2f}".format(women_calories_str)
+# men_calories = "{:.2f}".format(men_calories_str)
+#
+#
+# print('Women:', str(women_calories), 'calories')
+# print('Men:', str(men_calories), 'calories')
+
+
+# -- TODO NEW --
+
+''' Calories = ((Age x 0.2757) + (Weight x 0.03295) + (Heart Rate x 1.0781) — 75.4991) x Time / 8.368 '''
+
+
+''' Type your code here. '''
+
+age = int(input("How old are you? :"))
+weight = float(input("What is your weight? :"))
+heartRate = float(input("What is your heart rate? :"))
+time = float(input("How many mins did you workout? :"))
+
+calories = ((age * 0.2757) + (weight * 0.03295) + (heartRate * 1.0781) - 75.4991) * time / 8.368
+
+print('Calories: {:.2f} calories'.format(calories))
---
+++ lab/simpleInput.py
@@ -0,0 +1,26 @@
+# # todo 1
+first_name = input("Hello, what is your name? ")
+generic_location = input("Where are you shopping? ")
+whole_number = input("How many? ")
+plural_noun = input("What is it called again? ")
+
+
+# # todo 2
+# first_name = input()
+# generic_location = input()
+# whole_number = input()
+# plural_noun = input()
+
+# todo 3
+# first_name, generic_location,  whole_number,  plural_noun = input().split()
+#
+# sums=0.0
+# k=3
+# for w in range(k):
+#     try:
+#         sums = sums + float(input("Pleas input number " + str(w+1) + " "))
+#     except ValueError:
+#         print("Invalid Input")
+
+# Output a short story using the four input values. Do not modify the code below.
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
