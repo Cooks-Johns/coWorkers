083d3a365a9dda2d4c30e0a601a8a6cf123883d3
Cooks-Johns <cooks-johns@gmail.com>
2022-01-09 23:10:43 -0600

    Removed whitespaces and just need to put in into a loop and repeat on invalid input

--- lab/checker.py
+++ lab/checker.py
@@ -1,29 +1,36 @@
 from datetime import date
 
 
+
+   # username = input("What is your name?")
+
+class color:
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
+
+def calculateYear(userAge):
+    if userAge <= 0:
+        return userAge
+    today = date.today()
+    year = today.year - userAge
+    return year
+
 while True:
 
-
-    username = input("What is your name?")
-    userAge = int(input("How old are you?"))
-
-    class color:
-        BOLD = '\033[1m'
-        END = '\033[0m'
-
-    def calculateYear(userAge):
-        today = date.today()
-        year = today.year - userAge
-        return year
-
-
+    username = input("What is your name? ").capitalize()
+    userAge = int(input("How old are you? "))
     boldYear = str(calculateYear(userAge))
 
+    hi = "Hello {}!"
+    age = "You were born in {}."
 
-    print("Hello", color.BOLD + username + color.END)
-    print("You were born in",  color.BOLD + boldYear + color.END, ".")
+    print(hi.format(color.BOLD + username + color.END),
+          "You were born in", age.format(color.BOLD + boldYear + color.END))
+    print()
 
 
 # TODO
 # TODO
-# TODO
+# TODO  firstUpper = username.capitalize()
+#  --
