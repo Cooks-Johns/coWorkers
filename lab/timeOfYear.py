0db0a2b760e01a319ae5f9b8ac70d2eed370645b
Cooks-Johns <cooks-johns@gmail.com>
2022-01-21 14:52:20 -0600

    making a backup

--- lab/timeOfYear.py
+++ lab/timeOfYear.py
@@ -1,23 +1,66 @@
-from time import strptime
-
-# user_month = (input("Enter number for month: ")
-# use_date = int(input("Enter number for date: "))
-month_input = input("Enter a month: ")
-user_date = int(input("Enter your date: "))
-# month_num = strptime(month_input, "%b").tm_mon
+# from time import strptime
 #
-# date = month_num, user_date
-# print(date)
-print(month_input)
+# # user_month = (input("Enter number for month: ")
+# # use_date = int(input("Enter number for date: "))
+# month_input = input("Enter a month: ")
+# user_date = int(input("Enter your date: "))
+# # month_num = strptime(month_input, "%b").tm_mon
+# #
+# # date = month_input, user_date
+# # # print(date)
+# # print(month_input)
+# #
+# if month_input == ("March" or "April" or "May" or "June"):
+#     if user_date <= 20:
+#         print("Spring")
+# elif month_input == ("June", "July", "August", "September"):
+#     if user_date <= 21:
+#         print("Summer")
+# elif month_input == ("September","October", "November", "December"):
+#     if user_date <= 22:
+#         print("Autumn")
+# elif month_input == ("December", "January", "Febuary", "March") and user_date < (19):
+#     if user_date <= 21:
+#        print("Winter")
+# else:
+#     print("Invalid ")
+#
+# Take 2
+from datetime import datetime
+#
+# month_input = 'September'  #input()
+# day_input = -1  # int(input())
+#
+# if month_input in ('January', 'February', 'March'):
+#     season = 'Winter'
+# elif month_input in ('April', 'May', 'June'):
+#     season = 'Spring'
+# elif month_input in ('July', 'August', 'September'):
+#     season = 'Summer'
+# elif month_input in ('September', 'October', 'November', 'December'):
+#     season = 'Autumn'
+#     if (month_input == 'March') and (day_input > 19 and day_input < 30):
+#         season = 'Spring'
+#     elif (month_input == 'June') and (day_input > 20 and day_input < 30):
+#         season = 'Summer'
+#     elif (month_input == 'September') and (day_input > 21 and day_input < 31):
+#         season = 'Autumn'
+#     elif (month_input == 'December') and (day_input > 1 and day_input < 30):
+#         season = 'Winter'
+#     else:
+#          season = "Invalid"
+#
+# print(season)
+#
+# Take 3
+import calendar
+yy = 1988
+mm = 6
+print(calendar.month(yy, mm))
 
-if month_input == ("March" or "April" or "May" or "June"):
-    print("Spring")
-elif month_input == ("June", "July", "August", "September"):
-    print("Summer")
-elif month_input == ("September", "October", "November", "December"):
-    print("Autumn")
-elif month_input == ("December", "January", "Febuary", "March") and user_date < (19):
-    print("Winter")
-else:
-    print("Invalid ")
+if month_input == {'January', 'February', 'March'}:
 
+    ("December", "January", "Febuary", "March")
+    ("September", "October", "November", "December")
+    ("September", "October", "November", "December")
+    ("March", "April", "May", "June")
