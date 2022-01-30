0dae497ff263378853fc4d4f415c0581553ca0f9
Cooks-Johns <cooks-johns@gmail.com>
2022-01-29 20:13:19 -0600

    loops and more

--- lab/crafTool/while.py
+++ lab/crafTool/while.py
@@ -270,3 +270,84 @@ Average: 5
 #         inner_helper += 1
 #     outer_helper += 1
 
+
+# =================================================================
+# #--- HangMan
+# word = 'onomatopoeia'
+# num_guesses = 10
+#
+# hidden_word = '-' * len(word)
+#
+# guess = 1
+#
+# while guess <= num_guesses and '-' in hidden_word:
+#     print(hidden_word)
+#     user_input = input('Enter a character (guess #{}): '.format(guess))
+#
+#     if len(user_input) == 1:
+#         # Count the number of times the character occurs in the word
+#         num_occurrences = word.count(user_input)
+#
+#         # Replace the appropriate position(s) in hidden_word with the actual character.
+#         position = -1
+#         for occurrence in range(num_occurrences):
+#             position = word.find(user_input, position + 1)  # Find the position of the next occurrence
+#             hidden_word = hidden_word[:position] + user_input + hidden_word[
+#                                                                 position + 1:]  # Rebuild the hidden word string
+#
+#         guess += 1
+#
+# if not '-' in hidden_word:
+#     print('Winner!', end=' ')
+# else:
+#     print('Loser!', end=' ')
+#
+# print('The word was {}.'.format(word))
+
+
+# =================================================================
+# #--- Passenger data
+# Fixme use this for something else it just needs a lil rework
+
+#
+# menu_prompt = ('Available commands:\n'
+#                '  (add) Add passenger\n'
+#                '  (del) Delete passenger\n'
+#                '  (print) Print passenger list\n'
+#                '  (exit) Exit the program\n'
+#                'Enter command:\n')
+#
+# destinations = ['PHX', 'AUS', 'LAS']
+#
+# destination_prompt = ('Available destinations:\n'
+#                       '(PHX) Phoenix\n'
+#                       '(AUS) Austin\n'
+#                       '(LAS) Las Vegas\n'
+#                       'Enter destination:\n')
+#
+# passengers = {}
+#
+# print('Welcome to Mohawk Airlines!\n')
+# user_input = input(menu_prompt).strip().lower()
+#
+# while user_input != 'exit':
+#     if user_input == 'add':
+#         name = input('Enter passenger name: \n').strip().upper()
+#         destination = input(destination_prompt).strip().upper()
+#         if destination not in destinations:
+#             print('Unknown destination. \n')
+#         else:
+#             passengers[name] = destination
+# # Delete
+#     elif user_input == 'del':
+#         name = input('Enter passenger name: \n').strip().upper()
+#         if name in passengers:
+#             del passengers[name]
+# # Print
+#     elif user_input == 'print':
+#         for passenger in passengers:
+#             print('{} --> {}'.format(passenger.title(),passengers[passenger]))
+#     else:
+#         print('Unrecognized command, please try again.')
+#
+#     user_input = input('Enter command:\n').strip().lower()
