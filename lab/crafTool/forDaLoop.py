8f0f90d95e392dea4dfdcd3de6a36479975b8e68
Cooks-Johns <cooks-johns@gmail.com>
2022-01-31 00:04:58 -0600

    last loop

--- lab/crafTool/forDaLoop.py
+++ lab/crafTool/forDaLoop.py
@@ -64,6 +64,166 @@
 #     print(new_email, 'is', new_contact)
 
 
+# =================================================================
+### NESTED LOOPS
+"""
+Program to print all 2-letter domain names.
 
-for i in range(0, -2, -1):
-    print(i, end=' ')
+Note that ord() and chr() convert between text and the ASCII or Unicode encoding:
+-  ord('a') yields the encoded value of 'a', the number 97.
+-  ord('a')+1 adds 1 to the encoded value of 'a', giving 98.
+-  chr(ord('a')+1) converts 98 back into a letter, producing 'b'
+"""
+# print('Two-letter domain names:')
+#
+# letter1 = 'a'
+# letter2 = '?'
+# while letter1 <= 'z':  # Outer loop
+#     letter2 = 'a'
+#     while letter2 <= 'z':  # Inner loop
+#         print('{}{}.com'.format(letter1, letter2))
+#         letter2 = chr(ord(letter2) + 1)
+#     letter1 = chr(ord(letter1) + 1)
+# =========
+# Histogram
+# num = 0
+# while num >= 0:
+#     num = int(input('Enter an integer (negative to quit) :\n'))
+#
+#     if num >= 0:
+#         print('Depicted graphically:')
+#         for i in range(num):
+#             print('*', end='')
+#         print('\n')
+#
+# print('Goodbye.')
+#
+# =========
+
+# c1 = 'a'
+# while c1 < 'b':
+#     c2 = 'a'
+#     while c2 <= 'c':
+#         print('{}{}'.format(c1, c2), end=' ')
+#         c2 = chr(ord(c2) + 1)
+#     c1 = chr(ord(c1) + 1)
+
+# =========
+#
+# i1 = 1
+# while i1 < 19:
+#     i2 = 3
+#     while i2 <= 9:
+#         print('{}{}'.format(i1,i2), end=' ')
+#         i2 = i2 + 3
+#     i1 = i1 + 10
+## ======
+# num_rows = 33
+# num_cols = 21
+#
+# for i in range(num_rows):
+#     print('*', end=' ')
+#     for j in range(num_cols-1):
+#         i*=j
+#
+#         print('*', end=' ')
+#     print()
+
+# =================================================================
+### Taco break
+#
+# empanada_cost = 3
+# taco_cost = 4
+#
+# user_money = int(input('Enter money for meal: '))
+#
+# max_empanadas = user_money // empanada_cost
+# max_tacos = user_money // taco_cost
+#
+# meal_cost = 0
+# for num_tacos in range(max_tacos + 1):
+#     for num_empanadas in range(max_empanadas + 1):
+#         meal_cost = (num_empanadas * empanada_cost) + (num_tacos * taco_cost)
+#
+#         # Find first meal option that exactly matches user money
+#         if meal_cost == user_money:
+#             break
+#
+#     # Find first meal option that exactly matches user money
+#     if meal_cost == user_money:
+#         break
+#
+# if meal_cost == user_money:
+#     print('${} buys {} empanadas and {} tacos without change.'
+#         .format(meal_cost, num_empanadas, num_tacos))
+# else:
+#     print('You cannot buy a meal without having change left over.')
+
+# # =======
+### Taco continue
+#
+# empanada_cost = 3
+# taco_cost = 4
+#
+# user_money = int(input('Enter money for meal: '))
+#
+# num_diners = int(input('How many people are eating: '))
+#
+# max_empanadas = user_money // empanada_cost
+# max_tacos = user_money // taco_cost
+#
+# meal_cost = 0
+# num_options = 0
+# for num_tacos in range(max_tacos + 1):
+#     for num_empanadas in range(max_empanadas + 1):
+#
+#         # Total items purchased must be equally divisible by number of diners
+#         if (num_tacos + num_empanadas) % num_diners != 0:
+#             continue
+#
+#         meal_cost = (num_empanadas * empanada_cost) + (num_tacos * taco_cost)
+#
+#         if meal_cost == user_money:
+#             print('${} buys {} empanadas and {} tacos without change.'
+#                   .format(meal_cost, num_empanadas, num_tacos))
+#             num_options += 1
+#
+# if num_options == 0:
+#     print('You cannot buy a meal without having change left over.')
+
+
+# ===============================================
+# Simon Says
+
+# user_score = 0
+# simon_pattern = input()
+# user_pattern  = input()
+#
+# simon = ('R','G','B','Y')
+#
+#
+#
+# print('User score:', user_score)
+
+
+# ===============================================
+choice = ''
+
+while choice != "quit":
+    while True:
+        print("1. Fun story")
+        
+        choice2 = int(input("Which template of madlib would you like to play (Enter the number of your choice) "))
+        break  # This break will take you out of this while loop to reach if/else
+
+    if choice2 == 1:
+       noun1 = input("Enter a noun: ")
+       plural_noun = input("Enter a plural noun: ")
+       noun2 = input("Enter another noun: ")
+       print("Be kind to your {}-footed {} a day keep the doctor away.".format(noun1, plural_noun, noun2))
+
+    else:
+        choice = "quit"
+
+
+# ===============================================
